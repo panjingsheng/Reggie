7aa65c505d1503c4bd0d61210bc85050a49a372f
“panjingsheng” <1766048875@qq.com>
2022-12-30 14:22:41 +0800

    11

---
+++ src/main/java/com/itheima/reggie/service/impl/DishFlavorServiceImpl.java
@@ -0,0 +1,9 @@
+package com.itheima.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.entity.DishFlavor;
+import com.itheima.reggie.mapper.DishFlavorMapper;
+import com.itheima.reggie.service.DishFlavorService;
+
+public class DishFlavorServiceImpl extends ServiceImpl<DishFlavorMapper, DishFlavor> implements DishFlavorService {
+}
