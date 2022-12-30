5b99ec749170e78d9a30b1ddb53f16a83b2ee0fa
“panjingsheng” <1766048875@qq.com>
2022-12-30 15:22:47 +0800

    12.2

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,13 +1,19 @@
 package com.itheima.reggie.controller;
 
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dish")
+@Slf4j
 public class DishController {
 @Autowired
     private DishService dishService;
@@ -15,5 +21,12 @@ public class DishController {
 @Autowired
     private DishFlavorService dishFlavorService;
 
+@PostMapping
+public R<String> save(@RequestBody DishDto dishDto){
+log.info(dishDto.toString());
+
+
+    return null;
+}
 
 }
--- src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
+++ src/main/java/com/itheima/reggie/service/impl/DishServiceImpl.java
@@ -1,13 +1,36 @@
 package com.itheima.reggie.service.impl;
 
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.entity.Dish;
+import com.itheima.reggie.entity.DishFlavor;
 import com.itheima.reggie.mapper.DishMapper;
+import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Slf4j
 public class DishServiceImpl extends ServiceImpl<DishMapper, Dish> implements DishService {
+@Autowired
+    private DishFlavorService dishFlavorService;
+
+    @Override
+    @Transactional
+    public void saveWithFlavor(DishDto dishDto) {
+        this.save(dishDto);
+        Long dishId = dishDto.getId();
+        List<DishFlavor> flavors = dishDto.getFlavors();
+        flavors= flavors.stream().map((item)->{item.setDishId(dishId);
+        return item;
+        }).collect(Collectors.toList());
+        dishFlavorService.saveBatch(flavors);
+
+    }
 }
